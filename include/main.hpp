3a6275515e12de5e110d8a5d7907e2cfe6e64bd6
Dragos Bajanica <bajanica.dragos@gmail.com>
2026-02-04 14:07:12 -0500

    Added time taken for initialization and simulation

--- include/main.hpp
+++ include/main.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
-#define SOLVER  SOLVER_POSITION_VERLET
-#define ENGINE  ENGINE_DIRECT_FORCE
-#define COUNT   500
-#define TIME    100.0
-#define STEP    0.01
+#define SOLVER      SOLVER_POSITION_VERLET
+#define ENGINE      ENGINE_DIRECT_FORCE
+#define COUNT       2000
+#define TIME        1000.0
+#define STEP        0.1
+
+#define CORE_COUNT  8
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include "main.hpp"
 #include "system.hpp"
+#include <omp.h>
 #include <memory>
+#include <chrono>
 #include <iostream>
 
 int main() {
+    omp_set_num_threads(CORE_COUNT);
     std::cout << "Graviton test with " << COUNT << " bodies\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
     std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>(SOLVER, ENGINE, COUNT);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Initialization took " << duration.count() << " s\n";
+
+    start = std::chrono::high_resolution_clock::now();
     cluster->runStats(TIME, STEP);
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    std::cout << "Simulation took " << duration.count() << " s\n";
+
     return 0;
 }
